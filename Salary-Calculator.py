df339841e9a06165e28c81ceca216530197607f9
hudso2tj <161397308+hudso2tj@users.noreply.github.com>
2024-05-13 21:20:28 -0400

    Add files via upload

---
+++ Salary-Calculator.py
@@ -0,0 +1,42 @@
+#Programmer name: Trevor Hudson
+#Purpose: To create a payroll program that takes data inputs and calculates net pay
+#My submission of this program indicates that I have neither received nor
+#given unauthorized assistance in writing this program.
+#All coding is my own work.
+# User input of name, hours worked, federal and state tax rates
+name = str(input("Enter employee's name (first and last): "))
+hours_worked = int(input("Enter the number of hours worked this week: "))
+hourly_rate = float(input("Enter hourly rate: "))
+fed_tax = float(input("Enter federal withholding rate(decimal): "))
+state_tax = float(input("Enter state tax withholding rate(decimal): "))
+#calculates pay before deductions
+gross_pay = hourly_rate * hours_worked
+print()
+print()
+print()
+#displays the user inputed name
+print(f'Employee Name: {name}')
+print()
+#Displays the user uinputed hours and hourly rate
+print(f'Hours: {hours_worked}')
+print(f'Hourly Rate: \t ${hourly_rate:>8}')
+#Prints the calculation of gross pay
+print(f'Gross Pay: \t ${gross_pay:>8.2f}')
+print()
+print("Deductions:")
+#Calculates the deduction of federal tax based on user input
+fedtax_total = fed_tax * gross_pay
+#Calculates state tax based on user input
+statetax_total = state_tax * gross_pay
+#displays the federal tax deduction 
+print(f'\tFederal Withholding {fed_tax:.1%}: \t ${fedtax_total:>7.2f}')
+#displays the state tax deduction
+print(f'\tState Withholding {state_tax:.1%}: \t ${statetax_total:>7.2f}')
+#Calculates the total deduction 
+total_deductions = fedtax_total + statetax_total
+print(f'\tTotal Deductions: \t\t ${total_deductions:>7.2f}')
+#Calculates net pay
+net_pay = gross_pay - total_deductions
+#displays net pay 
+print(f'Net pay: \t\t\t\t ${net_pay:>7.2f}\n')
+
