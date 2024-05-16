7677aacb99f3907d28043f345c29c826843d4e53
hudso2tj <161397308+hudso2tj@users.noreply.github.com>
2024-05-15 20:14:30 -0400

    Update Salary-Calculator.py

--- Salary-Calculator.py
+++ Salary-Calculator.py
@@ -4,11 +4,13 @@
 #given unauthorized assistance in writing this program.
 #All coding is my own work.
 # User input of name, hours worked, federal and state tax rates
-name = str(input("Enter employee's name (first and last): "))
+name = str(input("Enter employee's name: "))
 hours_worked = int(input("Enter the number of hours worked: "))
 hourly_rate = float(input("Enter hourly rate: "))
-fed_tax = float(input("Enter federal withholding rate(decimal): "))
-state_tax = float(input("Enter state tax withholding rate(decimal): "))
+fed_tax = float(input("Enter federal withholding rate %: "))
+state_tax = float(input("Enter state tax withholding %: "))
+fed_tax = fed_tax / 100
+state_tax = state_tax / 100
 #calculates pay before deductions
 gross_pay = hourly_rate * hours_worked
 print()
@@ -19,9 +21,9 @@ print(f'Employee Name: {name}')
 print()
 #Displays the user uinputed hours and hourly rate
 print(f'Hours: {hours_worked}')
-print(f'Hourly Rate: \t ${hourly_rate:>10.2f}')
+print(f'Hourly Rate: \t ${hourly_rate:>11,.2f}')
 #Prints the calculation of gross pay
-print(f'Gross Pay: \t ${gross_pay:>10.2f}')
+print(f'Gross Pay: \t ${gross_pay:>11,.2f}')
 print()
 print("Deductions:")
 #Calculates the deduction of federal tax based on user input
@@ -29,14 +31,14 @@ fedtax_total = fed_tax * gross_pay
 #Calculates state tax based on user input
 statetax_total = state_tax * gross_pay
 #displays the federal tax deduction 
-print(f'\tFederal Withholding {fed_tax:.1%}: \t ${fedtax_total:>10.2f}')
+print(f'\tFederal Withholding {fed_tax:.1%}: \t ${fedtax_total:>11,.2f}')
 #displays the state tax deduction
-print(f'\tState Withholding {state_tax:.1%}: \t ${statetax_total:>10.2f}')
+print(f'\tState Withholding {state_tax:.1%}: \t ${statetax_total:>11,.2f}')
 #Calculates the total deduction 
 total_deductions = fedtax_total + statetax_total
-print(f'\tTotal Deductions: \t\t ${total_deductions:>10.2f}')
+print(f'\tTotal Deductions: \t\t ${total_deductions:>11,.2f}')
 #Calculates net pay
 net_pay = gross_pay - total_deductions
 #displays net pay 
-print(f'\nNet pay: \t\t\t\t ${net_pay:>10.2f}\n')
+print(f'\nNet pay: \t\t\t\t ${net_pay:>11,.2f}\n')
 
